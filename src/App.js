48a5923223a545ebd59b29b3438bdc42486ad8dc
Tom Peng <tom.peng@arcticwolf.com>
2020-01-20 15:37:12 -0500

    reproduced

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import {Modal} from "@rtkwlf/ui";
+import {Field, Formik, withFormik} from "formik";
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+
+const noOp = () => {};
+
+const InnerModal = () => {
+  return (
+    <Modal visible footer="some footer" onCancel={noOp} onOk={noOp}>
+      <form>
+        <Field name={"input1"}/>
+        <Field name={"input2"}/>
+      </form>
+    </Modal>
+  );
+};
+
+// eslint-disable-next-line no-unused-vars
+const ModalForm = () => {
+  return (
+    <Formik initialValues={{input1: "hi", input2: "hey"}} onSubmit={noOp}>
+      <Modal visible footer="some footer" onCancel={noOp} onOk={noOp}>
+        <form>
+          <Field name={"input1"}/>
+          <Field name={"input2"}/>
+        </form>
+      </Modal>
+    </Formik>
+  )
+};
+
+const ModalFormHOC = withFormik({
+  enableReinitialize: true,
+  mapPropsToValues: () => ({input1: "hi", input2: "hey"}),
+  handleSubmit: noOp // handled by modal buttons
+})(InnerModal);
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +55,8 @@ function App() {
           Learn React
         </a>
       </header>
+      {/*<ModalFormHOC/>*/}
+      <ModalForm/>
     </div>
   );
 }
